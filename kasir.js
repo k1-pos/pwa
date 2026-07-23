fb1f52618a0b6055e05b5810478655442f79a434
Ahmad Shiddiq <shiddiq@gmail.com>
2026-07-23 14:55:17 +0700

    Menambahkan kasir.js

---
+++ kasir.js
@@ -0,0 +1,18 @@
+const addToCartLogic = (produk, keranjang) => {
+    if (produk.stok <= 0) return;
+    const item = keranjang.value.find(i => i.id === produk.id);
+    if (item) {
+        item.qty++;
+    } else {
+        keranjang.value.push({ id: produk.id, nama: produk.nama, harga: produk.harga, qty: 1 });
+    }
+    produk.stok--;
+};
+
+const changeQtyLogic = (item, change, products, removeItem) => {
+    const produkAsli = products.value.find(p => p.id === item.id);
+    if (change === 1 && produkAsli.stok <= 0) return;
+    item.qty += change;
+    produkAsli.stok -= change;
+    if (item.qty <= 0) removeItem(item);
+};
